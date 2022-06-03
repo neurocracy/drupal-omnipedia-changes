779ad55fdad77a2d4ebd87c93872d4b7fcff5b72
Matei Stanca <i@ambientimpact.com>
2022-06-03 19:10:36 -0400

    Renamed omnipedia_content_changes to omnipedia_changes;
    This also moves the module out of omnipedia_content.

---
+++ src/Event/Omnipedia/Changes/DiffPostRenderPreBuildEvent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\omnipedia_changes\Event\Omnipedia\Changes;
+
+use Drupal\omnipedia_changes\Event\Omnipedia\Changes\AbstractDiffEvent;
+
+/**
+ * Omnipedia changes diff post-render pre-build event.
+ *
+ * This event is dispatched after the current and previous wiki node revisions
+ * have been rendered, and before the diff is built, allowing alterations to
+ * the markup sent to the diffing service.
+ */
+class DiffPostRenderPreBuildEvent extends AbstractDiffEvent {
+}
